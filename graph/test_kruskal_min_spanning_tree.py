1aa4f635bffacd1b8f9ab4d4b998d1854f293f62
Kannan Paramasivam <kannan.paramasivam@live.comx>
2019-09-22 10:06:11 -0500

    kruskal

---
+++ graph/test_kruskal_min_spanning_tree.py
@@ -0,0 +1,33 @@
+import unittest
+from kruskal_min_spanning_tree import Graph
+
+
+class TestMinSpanningTree(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_prims_should_give_min_spanning_tree(self):
+        g = Graph('a b c d e f g h i'.split())
+        g.add_edge('a', 'b', 4)
+        g.add_edge('b', 'c', 8)
+        g.add_edge('c', 'd', 7)
+        g.add_edge('d', 'e', 9)
+        g.add_edge('e', 'f', 10)
+        g.add_edge('f', 'g', 2)
+        g.add_edge('g', 'h', 1)
+        g.add_edge('h', 'a', 8)
+        g.add_edge('b', 'h', 11)
+        g.add_edge('h', 'i', 7)
+        g.add_edge('c', 'i', 2)
+        g.add_edge('g', 'i', 6)
+        g.add_edge('c', 'f', 4)
+        g.add_edge('d', 'f', 14)
+        self.assertEqual(g.kruskal(), 37)
+
+
+if __name__ == '__main__':
+    unittest.main()
